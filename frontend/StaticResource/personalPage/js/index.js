fa5c4425883d53624c8edb165614758f0e7fcd61
GRAPECITY\LanceLi <Lance.li@grapecity.com>
2021-07-09 14:58:20 +0800

    add personal home page

---
+++ frontend/StaticResource/personalPage/js/index.js
@@ -0,0 +1,10 @@
+var personalCard = new Vue({
+    el: "#personalCard",
+    data: {
+        name: "lxc",
+        introduction: "",
+        photoPath: "../StaticResource/personalPage/img/photo.jpg"
+    }
+})
+
+personalCard.introduction = "躺平菜狗";
