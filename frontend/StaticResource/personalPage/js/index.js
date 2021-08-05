b3f58043914dd4d1e972998e2f8e7491ad82ba40
wuwang365 <1125344818@qq.com>
2021-08-05 22:34:14 +0800

    chnage request url

--- frontend/StaticResource/personalPage/js/index.js
+++ frontend/StaticResource/personalPage/js/index.js
@@ -14,8 +14,8 @@ var load = function(){
     getPersonalInfo("2018119085");
 }
 
-var getPersonalInfo = new function (userId) {
-    var url = "120.55.43.184:8080/information/getPersonal?userId="+userId;
+var getPersonalInfo = function (userId) {
+    var url = "http://127.0.0.1:3036/information/getPersonal?userId="+userId;
     var xhr = new XMLHttpRequest();
     xhr.open("GET",url,true);
     xhr.send();
@@ -27,7 +27,6 @@ var getPersonalInfo = new function (userId) {
             personalCard.photoPath = result.photoPath;
         }
     }
-    const baseData = { name: "李晓晨", introduction: "123", photoPath: "../StaticResource/personalPage/img/photo.jpg" }
     
 }
 
