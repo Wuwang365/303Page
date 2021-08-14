fb5711dab9edc904bd1e11fe4eb8715ca2451863
wuwang365 <1125344818@qq.com>
2021-08-14 16:12:59 +0800

    fix the style of person list

--- frontend/GlobalResource/js/index.js
+++ frontend/GlobalResource/js/index.js
@@ -1,13 +1,13 @@
 Vue.component("navbar", {
     template: `<div><nav class="navbar navbar-expand-lg navbar-light navbar-bg">
-    <a class="navbar-brand" href="#">
+    <a class="navbar-brand" href="./index.html">
         <img src="../GlobalResource/img/logo.jpg" width="45" height="60" class="d-inline-block align-center margin-length" alt="missing">
         <span class="tabbar-font">三零三所</span>
       </a>
     <div class="collapse navbar-collapse navbar-content-margin" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
             <li class="nav-item active">
-                <a class="nav-link" href="#">主页</a>
+                <a class="nav-link" href="./index.html">主页</a>
             </li>
             <li class="nav-item active">
                 <a class="nav-link" href="./personList.html">关于我们</a>
