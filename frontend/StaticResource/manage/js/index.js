a93048662ac95f16ecb2dc3f87cce766ebabfc7f
wuwang365 <1125344818@qq.com>
2021-08-17 00:08:56 +0800

    finish UI of manage

--- frontend/StaticResource/manage/js/index.js
+++ frontend/StaticResource/manage/js/index.js
@@ -1,19 +1,19 @@
 var load = () => {
     let tokenName = window.localStorage["tokenName"];
     let tokenValue = window.localStorage["tokenValue"];
-    let p = getRequest("http://127.0.0.1:3036/authority/manage/getInformation",tokenName,tokenValue);
-    p.then((text)=>{
-        if(text!=""&&texxt!=undefined){
+    let p = getRequest("http://127.0.0.1:3036/authority/manage/getInformation", tokenName, tokenValue);
+    p.then((text) => {
+        if (text != "" && text != undefined) {
             try {
                 let result = JSON.parse(text);
                 information.photoPath = result.photoPath;
                 information.name = result.userName;
-                information.introduction = result.information.split("\\n");
-                information.direction = result.direction.split("\\n");
-                information.achievement = result.achievement.split("\\n");
-                information.education = result.education.split("\\n");
+                information.introduction = result.information;
+                information.direction = result.direction;
+                information.achievement = result.achievement;
+                information.education = result.education;
             } catch (error) {
-                
+                alert("error");
             }
         }
     });
@@ -24,9 +24,23 @@ var information = new Vue({
     data: {
         photoPath: "",
         name: "",
-        introduction: [],
-        direction: [],
-        achievement: [],
-        education: []
+        introduction: "",
+        direction: "",
+        achievement: "",
+        education: ""
     }
 });
+var area = document.getElementById("input-area");
+document.getElementById("introduction").onclick = () => {
+    area.value = information.introduction.replaceAll("\\n", "\n");
+}
+document.getElementById("achievement").onclick = () => {
+    area.value = information.achievement.replaceAll("\\n", "\n");
+}
+document.getElementById("education").onclick = () => {
+    area.value = information.education.replaceAll("\\n", "\n");
+}
+document.getElementById("direction").onclick = () => {
+    area.value = information.direction.replaceAll("\\n", "\n");
+}
+load();
