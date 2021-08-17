272c9d4ae6485c1130fa79c84f0894e091995fde
Wuwang365 <1125344818@qq.com>
2021-08-17 17:10:17 +0800

    finish frontend manage logic

--- frontend/GlobalResource/js/util.js
+++ frontend/GlobalResource/js/util.js
@@ -1,9 +1,9 @@
-var getRequest = (url,headerKey="",headerValue="")=>{
+var getRequest = (url, headerKey = "", headerValue = "") => {
     return new Promise((resolve, reject) => {
         var xhr = new XMLHttpRequest();
         xhr.open("GET", url, true);
-        if(headerKey!=""&&headerKey!=undefined){
-            xhr.setRequestHeader(headerKey,headerValue);
+        if (headerKey != "" && headerKey != undefined) {
+            xhr.setRequestHeader(headerKey, headerValue);
         }
         xhr.send();
         xhr.onreadystatechange = function () {
@@ -12,4 +12,21 @@ var getRequest = (url,headerKey="",headerValue="")=>{
             }
         }
     });
+}
+
+var postRequest = (url, body, headerKey = "", headerValue = "") => {
+    return new Promise((resolve, reject) => {
+        var xhr = new XMLHttpRequest();
+        xhr.open("POST", url, true);
+        if (headerKey != "" && headerKey != undefined) {
+            xhr.setRequestHeader(headerKey, headerValue);
+        }
+        xhr.setRequestHeader("Content-Type", "application/json");
+        xhr.send(body);
+        xhr.onreadystatechange = () => {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                resolve(xhr.responseText);
+            }
+        }
+    })
 }
--- frontend/StaticResource/manage/js/index.js
+++ frontend/StaticResource/manage/js/index.js
@@ -27,20 +27,55 @@ var information = new Vue({
         introduction: "",
         direction: "",
         achievement: "",
-        education: ""
+        education: "",
+        location: ""
     }
 });
 var area = document.getElementById("input-area");
 document.getElementById("introduction").onclick = () => {
     area.value = information.introduction.replaceAll("\\n", "\n");
+    information.location = "introduction";
 }
 document.getElementById("achievement").onclick = () => {
     area.value = information.achievement.replaceAll("\\n", "\n");
+    information.location = "achievement";
 }
 document.getElementById("education").onclick = () => {
     area.value = information.education.replaceAll("\\n", "\n");
+    information.location = "education";
 }
 document.getElementById("direction").onclick = () => {
     area.value = information.direction.replaceAll("\\n", "\n");
+    information.location = "direction";
+}
+document.getElementById("update").onclick = () => {
+    switch (information.location) {
+        case "introduction":
+            information.introduction = area.value;
+            break;
+        case "achievement":
+            information.achievement = area.value;
+            break;
+        case "education":
+            information.education = area.value;
+            break;
+        case "direction":
+            information.direction = area.value;
+            break;
+        default:
+            break;
+    }
+    var json = {
+        "information": information.introduction.replaceAll("\n", "\\n"),
+        "achievement": information.achievement.replaceAll("\n", "\\n"),
+        "education": information.education.replaceAll("\n", "\\n"),
+        "direction": information.direction.replaceAll("\n", "\\n")
+    }
+    var body = JSON.stringify(json);
+    var p = postRequest("http://127.0.0.1:3036/authority/manage/updateInformation",
+        body, window.localStorage["tokenName"], window.localStorage["tokenValue"]);
+    p.then((text)=>{
+        alert(text);
+    })
 }
 load();
